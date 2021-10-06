d460ce97c547111ee4005fe464f3208e1a8216bd
chokokiwiberry <kusamdevilal@gmail.com>
2021-10-06 08:39:07 +0200

    updates

--- api/server.js
+++ api/server.js
@@ -30,18 +30,43 @@ app.post('/heroes', (req, res) => {
     fs.writeFileSync('../heroes.json', JSON.stringify(heroes));
 })
 
+//per modificare un singolo eroe 
+app.put('/heroes/:id', function (req, res){
+    //if hero with id exits, if it exists check 
+    let found;
+    for (var i=0; i<heroes.length; i=i+1){
+        if ((parseInt(req.body.id) === (parseInt(heroes[i].id)))){
+            found = true;
+        }
+    }
+    if (found){
+        const updated = {
+            id: req.body.id,
+            name: req.body.name,
+            age: req.body.age,
+            gender: req.body.gender,
+            signs: req.body.signs,
+            notes: req.body.notes
+        }
+        //aggiorno l'eroe in questione
+
+    }
+   
+})
+
 //per cancellare un eroe
 app.delete('/heroes/:id', function (req, res) {
 
-    var indexOfCouseInJson = heroes.map(function (item) { return item.id; }).indexOf(parseInt(req.params.id)); //find the index of :id
-    if (indexOfCouseInJson === -1) {
+    var index = heroes.map(function (item) { return item.id; }).indexOf(parseInt(req.params.id)); //find the index of :id
+    if (index === -1) {
         res.statusCode = 404;
         return res.send('Error 404: No quote found');
     }
-
-    var result = heroes.splice(indexOfCouseInJson, 1);
-    console.log('dopo delete', result)
-    fs.writeFile('../heroes.json', JSON.stringify(result), function (err) {
+    
+    console.log('ho trovato index to remove', index);
+    heroes.splice(index, 1);
+    console.log('sono heroes dopo splice',heroes);
+    fs.writeFile('../heroes.json', JSON.stringify(heroes), function (err) {
         if (err) throw err;
         res.json(true);
     });
